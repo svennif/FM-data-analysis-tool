7ebd67109b944bc9ffbf479f17955069f2f7ace5
Sven Ingar Frantzen <sven.ingar@gmail.com>
2023-11-15 20:34:11 +0100

    Moved some files and testing

--- data-reader.py
+++ data-reader.py
@@ -1,4 +1,20 @@
-# Goal: I want to make a data analysis of the data from the stats of players in Football Manager 2024
-# I want to use the tool to find the best players for my team based of key stats in each role
-# Based of these key stats,  I want to rate the players from 1 to 200 and find the one that is best suited for my team 
+import pandas as pd
+import glob
+import os
 
+# Import files
+imported_files = glob.glob(os.path.join('D:\web\FM-Data-Analysis-Tool\data', "*"))
+
+if len(imported_files) > 0:
+    newest_file = max(imported_files, key=os.path.getctime)
+    print(newest_file)
+
+    # Read the file
+    raw_data_list = pd.read_html(newest_file, header=0, encoding="utf-8", keep_default_na=False)
+
+    # Create a dataframe
+    raw_data = raw_data_list[0]
+
+    print(raw_data['Name'])
+else:
+    print("No files to process.")
---
+++ roles/gk.py
@@ -0,0 +1,7 @@
+# Gk stats 
+
+# Save Ratio %
+# xG Prevented per 90
+# Blocks per 90
+# Pass completion %
+
