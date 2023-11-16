d2b9961356ff7a1697756c879829d90b34a2547b
Sven Ingar Frantzen <sven.ingar@gmail.com>
2023-11-16 21:53:36 +0100

    stubbe changes

---
+++ data-reader-stubbe.py
@@ -0,0 +1,16 @@
+import glob, os, sys
+# from bs4 import BeautifulSoup
+
+# Import files
+data_path = os.path.join(os.path.join(os.path.dirname(__file__), "data"), "*.html")
+
+file_contents =  glob.glob(data_path)
+
+for file in file_contents:
+    try:
+        with open(file, 'r') as f:
+            # Open file with Read 
+            html_text = f.read()
+            print(html_text)
+    except Exception  as  e:
+        print(e)
