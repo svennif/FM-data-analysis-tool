e313220501307a807f039e804339ee85decd13da
Sven Ingar Frantzen <sven.ingar@gmail.com>
2023-11-20 22:40:36 +0100

    FOOTBALL MANAGER ATTRIBUTES

---
+++ attributes_for_roles.py
@@ -0,0 +1,115 @@
+# Calculations I want to make:
+# Key attributes for all roles:
+    # Determination
+    # Anticipation
+    # Decisions
+
+# I want to calculate different areas: 
+    # Goalkeeping
+        # Aerial Reach
+        # Command of Area (K)
+        # Communication
+        # First Touch
+        # Handling
+        # Kicking (K)
+        # One on Ones (K)
+        # Passing
+        # Reflexes (K)
+        # Rushing Out
+        # Throwing
+        # Anticipation (K)
+        # Composure
+        # Concentration (K)
+        # Decisions 
+        # Positioning (K)
+        # Vision
+        # Acceleration
+        # Agility (K)
+
+    # Defending
+        # Centre Back
+            # Stopper
+                # Heading (K)
+                # Marking
+                # Tackling (K)
+                # Aggression (K)
+                # Anticipation
+                # Bravery (K)
+                # Composure
+                # Concentration
+                # Decisions (K)
+                # Positioning (K)
+                # Jumping Reach (K)
+                # Strength (K)
+
+            # Ball Playing
+                # First Touch
+                # Heading (K)
+                # Marking (K)
+                # Passing (K)
+                # Tackling (K)
+                # Technique
+                # Aggression
+                # Anticipation
+                # Bravery
+                # Composure (K)
+                # Concentration
+                # Decisions
+                # Positioning (K)
+                # Vision
+                # Jumping Reach (K)
+                # Pace
+                # Strength (K)
+
+        # Full Back
+            # Crossing (K)
+            # Dribbling
+            # First Touch
+            # Marking (K)
+            # Passing
+            # Tackling (K)
+            # Technique
+            # Anticipation (K)
+            # Concentration
+            # Decisions
+            # Off the Ball
+            # Positioning (K)
+            # Teamwork (K)
+            # Work Rate
+            # Agility
+            # Pace
+            # Stamina
+        # Wing Back
+            # Crossing (K)
+            # Dribbling (K)
+            # First Touch
+            # Marking (K)
+            # Passing
+            # Tackling (K)
+            # Technique
+            # Anticipation
+            # Concentration
+            # Decisions
+            # Off the Ball (K)
+            # Positioning 
+            # Teamwork (K)
+            # Work Rate (K)
+            # Acceleration (K)
+            # Agility
+            # Balance
+            # Pace
+            # Stamina (K)
+
+    # Midfield
+        # Destroyer
+        # Playmaker
+            # Deep Lying
+            # Advanced
+
+    # Attacking
+        # Winger 
+            # Provider
+            # Goalscorer
+        # Striker
+            # Provider
+            # Goalscorer
