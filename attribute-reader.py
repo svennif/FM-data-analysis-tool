5f1f324a14a143c1e921ee118d2defa11b264862
Sven Ingar Frantzen <sven.ingar@gmail.com>
2023-11-26 19:21:55 +0100

    Just some janitor work

--- attribute-reader.py
+++ attribute-reader.py
@@ -27,20 +27,13 @@ bbm_headers_key = ['Pas', 'Tck', 'OtB', 'Tea', 'Wor', 'Sta']
 for file in file_contents:
     try:
         with open(file, "r") as f:
-            # Open file with Read
             html_text = f.read()
             soup = BeautifulSoup(html_text, "html.parser")
-            # Find all table rows
             rows = soup.find_all("tr")
-            # Find all headers and strip the text.
             headers = [s.text for s in rows[0].find_all("th")]
-            # Print all headers
             print(headers)
-            # Remove the first row (headers)
             rows = rows[1:]
-            # Loop through all rows and create a dictionary
             for row in rows:
-                # Finds all table data and strips the text
                 row = [s.text for s in row.find_all("td")]
                 output = {}
                 # Loop through all headers and add to dictionary
@@ -60,30 +53,6 @@ for file in file_contents:
             # with open(f"{file}.json", "w") as f:
             #     # Write the dictionary to the file
             #     f.write(json.dumps(dict_list))
-
-            # I want to loop through the data of mezzala_headers_key, and multiply the value by 2 and sum everything together
-            # Then I want to take the values from mezzala_headers and sum them together
-            # Then I want to add the two sums together and divide by 46
-            
-            # Loop through all rows and create a dictionary
-            for row in rows:
-                # Finds all table data and strips the text
-                row = [s.text for s in row.find_all("td")]
-                output = {}
-                # Loop through all headers and add to dictionary
-                for idx, h in enumerate(headers):
-                    # Check if the header is in the selected headers list
-                    if h in mezzala_headers:
-                        # Add the header as the key and the row data as the value
-                        output[h] = row[idx]
-                # Add the dictionary to the list
-                dict_list.append(output)
-                # Print the dictionary
-                # print(json.dumps(dict_list))
-                # Write the dictionary to a json file
-                with open(f"{file}.json", "w") as f:
-                    # Write the dictionary to the file
-                    f.write(json.dumps(dict_list))
 
     except Exception as e:
         print(e)
